82959c3340f260b413bae1a24ecf76813ee554ed
wi11liamFernandez <william.fernandez@edu.itspiemonte.it>
2025-02-11 16:24:03 +0100

    adding Vessel class

---
+++ src/Application/entities/Vessel.cs
@@ -0,0 +1,21 @@
+﻿using Application.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.entities
+{
+    internal class Vessel: IVessel
+    {
+        public int Id { get; }
+        public string ImoNumber { get; set; }
+
+        public Vessel(string imoNumber) 
+        {
+            this.ImoNumber = imoNumber;
+        }
+
+    }
+}
