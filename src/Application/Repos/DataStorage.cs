b566586740ea53021f18c50a1a32841fe5d4b8f5
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-12 09:11:47 +0100

    Creation of the repo DataStorage

---
+++ src/Application/Repos/DataStorage.cs
@@ -0,0 +1,17 @@
+﻿using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repos
+{
+    /*
+     *  Creation of the list that will contain the vessels data 
+    */
+    public class DataStorage
+    {
+        public static List<Vessel> vessels = new List<Vessel>();
+    }
+}
