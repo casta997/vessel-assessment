114a9a4aeaaccd1292314c526618f84a6136f952
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-12 09:03:23 +0100

    Creation of the model Vessel

---
+++ src/Application/Models/Vessel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    /*
+     *   Creation of the model Vessel, with the following fields: _ id (int)
+     *                                                            _ imoNumber (string)
+    */
+    public class Vessel
+    {
+        public int Id { get; set; }
+        public string ImoNumber { get; set; }
+    }
+}
