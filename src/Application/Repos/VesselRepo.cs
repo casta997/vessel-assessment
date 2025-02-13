e2ef258488545578a7b0522966c0701f3160c645
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-13 12:30:23 +0100

    Created the repo VesselRepo

---
+++ src/Application/Repos/VesselRepo.cs
@@ -0,0 +1,15 @@
+﻿using Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repo
+{
+    //Creation of the vessels list
+    public class VesselRepo
+    {
+        public static List<VesselRepo> vessels = new();
+    }
+}
