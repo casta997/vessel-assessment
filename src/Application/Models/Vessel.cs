b3cbb5dd7b83fb954c0b903fa31dec52d85a82bc
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-14 15:08:29 +0100

    Creation of the Vessel model in the Models folder

---
+++ src/Application/Models/Vessel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    //Vessel entity 
+    public class Vessel
+    {
+        //Vessel properties
+        public int Id { get; set; }
+        public string ImoNumber { get; set; }
+
+        //Vessel constructor
+        public Vessel(int id, string imoNumber)
+        {
+            Id = id;
+            ImoNumber = imoNumber;
+        }
+
+        //Vessel custom string 
+        public override string ToString()
+        {
+            return $"{Id}\t{ImoNumber}";
+        }
+    }
+}
