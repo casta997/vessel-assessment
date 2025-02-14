b06188be470f4622a65a56fddbc2fe56c0ce1700
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-14 15:18:48 +0100

    Created the interface IVesselService in the Interfaces folder

---
+++ src/Application/Interfaces/IVesselService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    //Vessel interface
+    public interface IVesselService
+    {
+        //Methods that will be implemented in the VesselService
+        public void CreateVessel();
+        public void CreateSomeVessel();
+        public void ReadVessel();
+        public void UpdateVessel();
+        public void DeleteVessel();
+        public bool SelectActionOnVessel();
+    }
+}
