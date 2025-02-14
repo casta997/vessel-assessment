427fe51a49366f606db317991aff7b231c39e906
alessandra <alessandra.pedrotto2015@gmail.com>
2025-02-14 15:13:45 +0100

    Created the repository VesselRepo in the folder Repos

---
+++ src/Application/Repos/VesselRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repos
+{
+    //Vessel repository 
+    public class VesselRepo
+    {
+        //List that contains vessels objects, is called "vessels"
+        public static List<Vessel> vessels = new();
+    }
+}
